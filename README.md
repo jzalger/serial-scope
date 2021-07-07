1dd367857012a72ae54d1ae2a0628361a8e8e471
jzalger <jzalger@users.noreply.github.com>
2021-07-07 19:28:57 -0400

    Plotting implementation, but bugs with updating outside main thread.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # serial-scope
+[![Bugs](https://sonarcloud.io/api/project_badges/measure?project=jzalger_serial-scope&metric=bugs)](https://sonarcloud.io/dashboard?id=jzalger_serial-scope)
+
 Python serial visualizer for CSV formatted data streams
--- serialscope.py
+++ serialscope.py
@@ -1,23 +1,39 @@
 # This module defines functionality for the deepsix interface device
 import getopt
+from numpy import ceil, mod
 import serial
 import threading
 import pandas as pd
 import pylab as plt
 from sys import argv, exit
+from serial.serialutil import SerialException
 
 
 class SerialScope:
 
-    def __init__(self, serial_port='/dev/tty.usbserial-0221E24A', serial_baud=9600, headers=None, serial_log_limit=1000) -> None:
+    def __init__(self, serial_port=None, serial_baud=None, headers=None, serial_log_limit=1000) -> None:
+        parsed_headers = headers.split(",")
+        self.indicies = None
         self.serial_port = serial_port
         self.serial_baud = serial_baud
+        self.serial_log_limit = serial_log_limit
         self._monitoring_serial = False
         self._serial_thread = None
-        self._x_axis_header = headers[0]
-        self._headers = headers
-        self._data_headers = headers[1:]
-        self._buffer = pd.DataFrame(columns=headers)
+        self._x_axis_header = parsed_headers[0]
+        self._headers = parsed_headers
+        self._data_headers = parsed_headers[1:]
+        self._buffer = pd.DataFrame(columns=parsed_headers)
+        
+        # plt.ion()
+        if len(self._data_headers) <= 3:
+            rows, cols = 1, len(self._data_headers)
+        else:
+            div, rm = divmod(len(self._data_headers), 3)
+            rows, cols = div + rm, 3
+        self.fig, self.axes = plt.subplots(rows, cols)
+        plots = [p.plot([],[])[0] for p in self.axes.flat]
+        self._header_plot_map = {h:i for h,i in zip(self._data_headers, plots)}
+        plt.show()
 
     def start_monitoring_serial(self):
         self.serial_thread = threading.Thread(target=self._monitor_serial).start()
@@ -28,17 +44,21 @@ class SerialScope:
         self._monitoring_serial = False
 
     def flush_serial_log(self):
-        self.serial_log = list()
+        pass
 
-    def _monitor_serial(self, msg_handlers) -> None:
-        while True:
-            with serial.Serial(self.serial_port, self.serial_baud, timeout=5) as hardware_serial:
-                msg = hardware_serial.readline()
-                data = self._parse_msg(msg)
-                self.add_data(data)
-                self.update_plot()
-                if len(self.serial_log) > self.serial_log_limit:
-                    self.flush_serial_log()
+    def _monitor_serial(self) -> None:
+        try:
+            with serial.Serial(self.serial_port, self.serial_baud, timeout=1) as hardware_serial:
+                while True:
+                    msg = hardware_serial.readline()
+                    data = self._parse_msg(msg.decode('utf-8'))
+                    self.add_data(data)
+                    self.update_plot()
+                    if len(self._buffer) > self.serial_log_limit:
+                        self.flush_serial_log()
+        except SerialException:
+            print("Could not open that serial port (%s) or baud (%s)", self.serial_port, self.serial_baud)
+            exit(2)
     
     def _parse_msg(self, msg) -> list:
         # FIXME: manage this more generically
@@ -47,42 +67,38 @@ class SerialScope:
         return data
 
     def add_data(self, data):
-        new_df = pd.DataFrame(data, columns=self._headers)
+        new_df = pd.DataFrame(data, index=self._headers)
         pd.concat([self._buffer, new_df])
 
-    def create_plot(self):
-        plt.ion()
-        X = self._buffer([self._x_axis_header])
-        for header, values in self._buffer(self._data_headers).iteritems():
-            plt.plot(X, values.values(), label=header)
-
     def update_plot(self):
-        # self.graph.set_y_data(self.buffer)
+        X = self._buffer[self._x_axis_header]
+        for header, values in self._buffer[self._data_headers].iteritems():
+            # plt.plot(X, values.values(), label=header)
+            axis = self._header_plot_map[header]
+            axis.set_data(X, values)
         plt.draw()
-        plt.pause(0.01)
+        # plt.pause(0.01)
 
 
 def parse_opts(args):
     try:
-        opts, args = getopt.getopt(args, "p:b:", ["headers="])
+        opts, args = getopt.getopt(args, "p:b:", ["headers=","indicies="])
+        parsed = dict()
+        for opt, arg in opts:
+            if opt == '-p':
+                parsed["port"] = arg
+            elif opt == '-b':
+                parsed["baud"] = arg
+            elif opt == '--headers':
+                parsed["headers"] = arg         
+            # TODO: Add verification for parameters
+        return parsed
     except getopt.GetoptError:
-        print_help
+        print_help()
         exit(2)
-    parsed = dict()
-    for opt, arg in opts:
-        if opt == '-p':
-            parsed["port"] = arg
-        elif opt == '-b':
-            parsed["baud"] = arg
-        elif opt == '--headers':
-            parsed["headers"] = arg
-        else:
-            print_help
-            exit(2) 
-    return parsed
 
 def print_help():
-    print('serialscope.py -p <serial_port> -b <serial_baud> -h <header1,header2,..,headern>. Assumes value in first index is X axis.')
+    print('serialscope.py -p <serial_port> -b <serial_baud> --headers <header1,header2,..,headern> Assumes value in first index is X axis.')
 
 def main(args):
     parsed = parse_opts(args)
